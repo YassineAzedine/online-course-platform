9ec1ba7f810371dc44c3fe6b68ff7267ba4b47de
yassine azedine <yassine.azedine0711@gmail.com>
2025-02-05 21:47:27 +0100

    test automation deploye from backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!  i m online';
+    return 'hello world i automatically updated';
   }
 }
